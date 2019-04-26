40abceb989fb920b4bb4e211c739ef0dc57ccdb7
Marco Catalano <m.catalano12@campus.unimib.it>
2019-04-26 13:27:27 +0200

    Refact logger

--- src/main/scala/com/betway/Scraper.scala
+++ src/main/scala/com/betway/Scraper.scala
@@ -1,6 +1,7 @@
 package com.betway
 
 import akka.actor.{ActorSystem, Props}
+import akka.event.slf4j.Logger
 import com.betway.actor.ScannerActor
 import com.surebetfinder.config.SureBetFinderConfiguration
 import com.surebetfinder.db.Postgres
@@ -8,17 +9,19 @@ import com.surebetfinder.db.Postgres
 object Scraper {
 
   private val configuration: SureBetFinderConfiguration = new SureBetFinderConfiguration().load("./betway.yml")
+  val logger = Logger(configuration.siteName)
 
   def main(args: Array[String]): Unit = {
 
     // create the ActorSystem
     val system: ActorSystem = ActorSystem("betway-scraper")
-    system.log.info("Creating Actorsystem...")
-    system.log.info(configuration.siteName + " scraper is starting...")
+
+    logger.info("Creating Actorsystem...")
+    logger.info(configuration.siteName + " scraper is starting...")
 
     // postgres initialization
     val postgres = new Postgres(configuration)
-    system.log.info("Connection to Postgres DB works fine!")
+    logger.info("Connection to Postgres DB works fine!")
 
     // create a new actor, then send messages
     val scanner = system.actorOf(Props(classOf[ScannerActor]), "scanner")
